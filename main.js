33c18df2d5527eb20834d909eb692f388e9ebdd0
mahimjani <janimahim04@gmail.com>
2024-11-26 13:18:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -22,3 +22,9 @@ document.querySelector('#app').innerHTML = `
 `
 
 setupCounter(document.querySelector('#counter'))
+
+// Disable right-click
+document.addEventListener('contextmenu', function (event) {
+  event.preventDefault();
+  alert("Right-click is disabled on this website.");
+});
